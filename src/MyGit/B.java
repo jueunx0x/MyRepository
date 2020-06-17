cd5e7b8c677735197d9b130c61a151aaf80dfa9f
jueunx0x <jueunx_x@naver.com>
2020-06-17 18:40:14 +0900

    First

--- src/MyGit/B.java
+++ src/MyGit/B.java
@@ -2,6 +2,6 @@ package MyGit;
 
 public class B {
 	public static void main(String[] args) {
-		System.out.println("My name is");
+		System.out.println("2");
 	}
 }
--- src/MyGit/C.java
+++ src/MyGit/C.java
@@ -2,6 +2,6 @@ package MyGit;
 
 public class C {
 	public static void main(String[] args) {
-		System.out.println("Ω≈¡÷¿∫");
+		System.out.println("3");
 	}
 }
